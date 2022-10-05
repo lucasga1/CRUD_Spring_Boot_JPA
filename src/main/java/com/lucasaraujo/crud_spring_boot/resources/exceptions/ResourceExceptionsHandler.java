ab299811028723610d11aa86737df93d31b67641
Lucas Araujo <lucas.gouvea@dbccompany.com.br>
2022-10-05 10:17:46 -0300

    Exception handling -Delete

--- src/main/java/com/lucasaraujo/crud_spring_boot/resources/exceptions/ResourceExceptionsHandler.java
+++ src/main/java/com/lucasaraujo/crud_spring_boot/resources/exceptions/ResourceExceptionsHandler.java
@@ -1,5 +1,6 @@
 package com.lucasaraujo.crud_spring_boot.resources.exceptions;
 
+import com.lucasaraujo.crud_spring_boot.services.exceptions.DatabaseException;
 import com.lucasaraujo.crud_spring_boot.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,14 @@ public class ResourceExceptionsHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found, try again another Id!";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> databaseError(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
