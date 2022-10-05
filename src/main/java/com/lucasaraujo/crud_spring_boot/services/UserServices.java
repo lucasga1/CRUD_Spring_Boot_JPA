f60fc41e7cbf6664189115738590dda865c39613
Lucas Araujo <lucas.gouvea@dbccompany.com.br>
2022-10-05 10:22:40 -0300

    Exception handling -Update

--- src/main/java/com/lucasaraujo/crud_spring_boot/services/UserServices.java
+++ src/main/java/com/lucasaraujo/crud_spring_boot/services/UserServices.java
@@ -4,6 +4,7 @@ import com.lucasaraujo.crud_spring_boot.entities.User;
 import com.lucasaraujo.crud_spring_boot.repositories.UserRepository;
 import com.lucasaraujo.crud_spring_boot.services.exceptions.DatabaseException;
 import com.lucasaraujo.crud_spring_boot.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserServices {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
